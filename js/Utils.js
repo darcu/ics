afd5a8f656eaf6d26d5ac188f60943b15a98e882
ujrep <stefan.perju@yahoo.com>
2014-10-25 00:02:46 +0300

    scrollto

--- js/Utils.js
+++ js/Utils.js
@@ -8,7 +8,7 @@ Utils.createDom = function(obj) {
 		events: obj['events'] || {}
 	};
 	var domElem = document.createElement(args['type']);
-	
+
 	for (var i in args.attributes) {
 		args.attributes.hasOwnProperty(i) && domElem.setAttribute(i, args.attributes[i]);
 	}
@@ -17,7 +17,7 @@ Utils.createDom = function(obj) {
 	}
 	var container = document.createDocumentFragment();
 	for (var i = 0, n = args.content.length; i < n; i += 1) {
-		args.content[i] && container.appendChild( typeof args.content[i] == "string" || typeof args.content[i] == "number" ? document.createTextNode(args.content[i]) : args.content[i]);
+		args.content[i] && container.appendChild(typeof args.content[i] == "string" || typeof args.content[i] == "number" ? document.createTextNode(args.content[i]) : args.content[i]);
 	}
 	container.appendChild(domElem);
 	return domElem;
--- js/functionality.js
+++ js/functionality.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(){
-	var items =	document.getElementsByClassName('item');
+document.addEventListener('DOMContentLoaded', function() {
+	var items = document.getElementsByClassName('item');
 
-	for(var i = 0, n = items.length; i < n; i++) {
+	for (var i = 0, n = items.length; i < n; i++) {
 		items[i].addEventListener('click', function(e) {
-			for(var j = 0, n = items.length; j < n; j++) {
+			for (var j = 0, n = items.length; j < n; j++) {
 				Utils.hasClass(items[j], 'selected') && Utils.removeClasses(items[j], 'selected', false);
 			}
+			window.scrollTo(0,  e.currentTarget.offsetTop + e.currentTarget.offsetHeight);
 			Utils.addClasses(e.currentTarget, 'selected');
 		});
 	}
-
- });
+});
