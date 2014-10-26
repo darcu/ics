e2e622e8cbc44ec828b1fc09a2f8315efe2e1952
darcu <darcu.ro@gmail.com>
2014-10-26 20:46:13 +0200

    file structure reorg

---
+++ js/Generics.js
@@ -0,0 +1,55 @@
+// this is the generic Item metadata holder
+var Item = function(file) {
+	this.file = file;
+	this.url = urlFromFile(file);
+	this.name = file.name;
+	this.size = file.size;
+	this.type = file.type;
+};
+
+// this is the generic UI Item holder
+// it holds a reference to the DOM element 
+var UiItem = function(i) {
+	// is the current element selected or not
+	var isSelected = false;
+	var deselListener;
+	var item;
+
+	var select = function() {
+		// first trigger the global select event to deselect all other elements
+		Event.trigger('select', 'done');
+
+		// set this element to selected and add the class
+		isSelected = true;
+		Dom.addClasses(item, 'selected');
+		calcPosition();
+	};
+
+	var deselect = function() {
+		if (isSelected) {
+			// set this element to deselected and remove the class
+			isSelected = false;
+			Dom.removeClasses(item, 'selected');
+		}
+	};
+
+	var toggle = function() {
+		isSelected ? deselect() : select();
+	};
+
+	var calcPosition = function() {
+		var goToPosition = item.offsetTop;
+		smoothScrollTo(goToPosition, 300);
+	};
+
+	// listener to deselect element if another is selected
+	deselListener = Event.add('select', 'done', deselect);
+
+	item = createItemDom(i, toggle);
+
+	calcImgHeight(i.url, function(aspect) {
+		var className = (aspect >= 1 ? 'hor' : 'vert');
+		Dom.addClasses(item.getElementsByTagName('img')[0], className);
+	});
+	this.dom = item;
+};
--- js/Utils.js
+++ js/Utils.js
@@ -2,3 +2,21 @@ function genRandomString() {
 	var sid = 1840724046193;
 	return Math.floor(Math.random() * sid).toString(36) + Math.abs(Math.floor(Math.random() * sid) ^ Date.now()).toString(36);
 }
+
+function urlFromFile(f) {
+	// here we assign a blobURL for the current file
+	var urlObj = URL || window.URL || webkitURL || window.webkitURL;
+	var url = urlObj.createObjectURL(f);
+
+	return url;
+}
+
+// we should get this straight from the img element
+function calcImgHeight(url, callback) {
+	var img = new Image();
+	img.onload = function() {
+		var aspect = (this.width * 100 / this.height) / 100;
+		callback(aspect)
+	}
+	img.src = url;
+}
